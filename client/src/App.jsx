e0e34ef99d18e6c7dcfa480a69cbcb1126ef6009
Patrick Tuyiringire <tpatrickrumanzi@gmail.com>
2025-02-24 10:39:44 -0600

    inital commit

---
+++ client/src/App.jsx
@@ -0,0 +1,47 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  HomeLayout,
+  Admin,
+  Login,
+  Register,
+  Profile,
+  DashboardLayout,
+  AllJobs,
+  DeleteJob,
+  EditJob,
+  Error,
+  Stats,
+  Landing,
+  AddJob,
+} from "./pages";
+
+const route = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: "register",
+        element: <Register />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "dashboard",
+        element: <DashboardLayout />,
+      },
+    ],
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={route} />;
+};
+export default App;
